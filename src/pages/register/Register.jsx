c20a11fbc0060bf902332ec653f372d1518e61a3
Auriukux <auriukux.info@gmail.com>
2025-01-03 18:28:00 +0200

    Update Register.jsx

--- src/pages/register/Register.jsx
+++ src/pages/register/Register.jsx
@@ -1,46 +1,101 @@
-//
+// Register.jsx
 import React, { useState } from 'react';
-import { getAuth, createUserWithEmailAndPassword } from 'firebase/auth';
-import '../../styles/user-register/Register.scss';
+import { useNavigate, Link } from 'react-router-dom';
+import '../../styles/register/register.scss';
+import { registerUser } from '../../actions/AuthActions';
 
 const Register = () => {
-  const [email, setEmail] = useState('');
-  const [password, setPassword] = useState('');
+  const [formData, setFormData] = useState({
+    name: '',
+    email: '',
+    password: ''
+  });
   const [error, setError] = useState(null);
+  const [isLoading, setIsLoading] = useState(false);
+  const navigate = useNavigate();
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setFormData(prevData => ({ ...prevData, [name]: value }));
+  };
 
   const handleRegister = async (event) => {
     event.preventDefault();
-    
-    const auth = getAuth();
+    setIsLoading(true);
     try {
-      await createUserWithEmailAndPassword(auth, email, password);
-      // Sėkmingai užregistruota, galite nukreipti naudotoją į kitą puslapį arba parodyti sėkmės pranešimą
-      alert('Registracija sėkminga!');
+      const result = await registerUser(formData.email, formData.password, formData.name);
+      if (result.success) {
+        navigate('/');
+      } else {
+        let errorMessage = 'Įvyko klaida: ';
+        if (result.error.includes('auth/email-already-in-use')) {
+          errorMessage = 'Šis el. paštas jau naudojamas.';
+        } else if (result.error.includes('Password should be at least 6 characters')) {
+          errorMessage = 'Slaptažodį turi sudaryti bent 6 simboliai.';
+        } else {
+          errorMessage += result.error;
+        }
+        setError(errorMessage);
+      }
     } catch (err) {
       setError(err.message);
+    } finally {
+      setIsLoading(false);
     }
   };
 
   return (
-    <form onSubmit={handleRegister} className="registration-form">
-      <h2>Registracija</h2>
-      <input 
-        type="email" 
-        value={email} 
-        onChange={(e) => setEmail(e.target.value)} 
-        placeholder="El. paštas" 
-        required 
-      />
-      <input 
-        type="password" 
-        value={password} 
-        onChange={(e) => setPassword(e.target.value)} 
-        placeholder="Slaptažodis" 
-        required 
-      />
-      <button type="submit">Registruotis</button>
-      {error && <p className="error">{error}</p>}
-    </form>
+    <div className='user-screen'>
+      <header className="header">
+        <h1>To Do List APP</h1>
+      </header>
+      <form onSubmit={handleRegister} className="user-form">
+        <h2>Registracija</h2>
+        <input 
+          type="text" 
+          name="name" 
+          value={formData.name} 
+          onChange={handleChange} 
+          placeholder="Vardas" 
+          required 
+          disabled={isLoading}
+        />
+        <input 
+          type="email" 
+          name="email" 
+          value={formData.email} 
+          onChange={handleChange} 
+          placeholder="El. paštas" 
+          required 
+          disabled={isLoading}
+        />
+        <input 
+          type="password" 
+          name="password" 
+          value={formData.password} 
+          onChange={handleChange} 
+          placeholder="Slaptažodis" 
+          required 
+          disabled={isLoading}
+        />
+        <button type="submit" disabled={isLoading}>
+          {isLoading ? 'Registruojama...' : 'Registruotis'}
+        </button>
+        {error && <p className="error">{error}</p>}
+        <div className="user-link">
+          <Link to="/login">Jau turite paskyrą? Prisijunkite</Link>
+        </div>
+        <div className="user-link">
+          <Link to="/">Grižti į pagridinį puslapį</Link>
+        </div>
+        {isLoading && (
+          <div className="loader-container">
+            <div className="loader"></div>
+            <span className="loader-text">Kraunama...</span>
+          </div>
+        )}
+      </form>
+    </div>
   );
 };
 
