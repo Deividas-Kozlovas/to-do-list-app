cb5c39448800334cf04d1772aceb3669a29dd000
Auriukux <auriukux.info@gmail.com>
2025-01-03 18:28:19 +0200

    Update App.jsx

--- src/components/app/App.jsx
+++ src/components/app/App.jsx
@@ -1,19 +1,25 @@
-//
-// import "./App.scss"; ???
+// App.jsx
+import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Login from "../../pages/login/Login";
+import { AuthProvider } from "../../context/AuthContext";
 import Register from "../../pages/register/Register";
+import Login from "../../pages/login/Login";
+import ResetPassword from '../../pages/password-reset/ResetPassword';
+import Home from "../../pages/home/Home";
+
 
 function App() {
   return (
-    <>
+    <AuthProvider>
       <Router>
         <Routes>
-          <Route path="/" element={<Login />} />
-          <Route path="/Register" element={<Register />} />
+          <Route path="/register" element={<Register />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/reset-password" element={<ResetPassword />} />
+          <Route path="/" element={<Home />} />
         </Routes>
       </Router>
-    </>
+    </AuthProvider>
   );
 }
 
