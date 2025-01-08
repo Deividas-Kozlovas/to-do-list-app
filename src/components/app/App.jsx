7926fc11f707f1a278a16bf83039548c52d0b8a3
Auriukux <auriukux.info@gmail.com>
2025-01-08 22:34:04 +0200

    Update env

--- src/components/app/App.jsx
+++ src/components/app/App.jsx
@@ -6,7 +6,6 @@ import Login from "../../pages/login/Login";
 import ResetPassword from "../../pages/password-reset/ResetPassword";
 import Home from "../../pages/home/Home";
 
-
 function App() {
   return (
     <Router>
